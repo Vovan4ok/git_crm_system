93a6763e96660ac62f563efc2e08497a84951e6f
Volodymyr Zganiaiko <vladimirzganiaiko@gmail.com>
2026-07-18 20:27:18 +0300

    - Add transaction ID and REST call logging via servlet filter

--- src/main/java/org/volodymyrzganiaiko/gym/crm/system/WebApplication.java
+++ src/main/java/org/volodymyrzganiaiko/gym/crm/system/WebApplication.java
@@ -3,11 +3,15 @@ package org.volodymyrzganiaiko.gym.crm.system;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.FilterDef;
+import org.apache.tomcat.util.descriptor.web.FilterMap;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.volodymyrzganiaiko.gym.crm.system.config.AppConfig;
 import org.volodymyrzganiaiko.gym.crm.system.config.WebConfig;
+import org.volodymyrzganiaiko.gym.crm.system.filter.TransactionIdFilter;
 
+import javax.servlet.Filter;
 import java.util.Locale;
 
 public class WebApplication {
@@ -23,6 +27,15 @@ public class WebApplication {
         tomcatContext.setParentClassLoader(Thread.currentThread().getContextClassLoader());
         Tomcat.addServlet(tomcatContext, "dispatcher", new DispatcherServlet(context)).setLoadOnStartup(1);
         tomcatContext.addServletMappingDecoded("/",  "dispatcher");
+        Filter filter = new TransactionIdFilter();
+        FilterDef filterDef = new FilterDef();
+        filterDef.setFilterName("TransactionIdFilter");
+        filterDef.setFilter(filter);
+        FilterMap filterMap = new FilterMap();
+        filterMap.setFilterName("TransactionIdFilter");
+        filterMap.addURLPattern("/*");
+        tomcatContext.addFilterDef(filterDef);
+        tomcatContext.addFilterMap(filterMap);
         tomcat.start();
         tomcat.getServer().await();
     }
